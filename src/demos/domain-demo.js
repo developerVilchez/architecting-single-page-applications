8cd448704bb528a9ac7144e9965573430911c57e
Daniel Dughila <daniel.dughila@orange.com>
2018-03-05 15:32:37 +0200

    add article state

--- src/demos/domain-demo.js
+++ src/demos/domain-demo.js
@@ -1,23 +1,29 @@
-import {articleService} from "./ArticleService";
+// @flow
+import {articleService} from "../domain/ArticleService";
 
 const articleServiceInstance = articleService();
 const article = articleServiceInstance.createArticle({
   title: '12 rules for life',
   author: 'Jordan Peterson'
 });
-const incrementedArticle = articleServiceInstance.updateLikes(article, 4);
+const incrementedArticle = article ? articleServiceInstance.updateLikes(article, 4) : null;
 
 console.log('article', article);
+/*
 const itWillPrint = {
   id: "92832a9a-ec55-46d7-a34d-870d50f191df",
   likes: 0,
   title: "12 rules for life",
   author: "Jordan Peterson"
 };
+*/
+
 console.log('incrementedArticle', incrementedArticle);
+/*
 const itWillPrintUpdated = {
   id: "7ddcdee8-09f1-474d-8486-a7a1bcdcbd34",
   likes: 4,
   title: "12 rules for life",
   author: "Jordan Peterson"
 };
+*/
---
+++ src/state/ArticleSateService.js
@@ -0,0 +1,51 @@
+// @flow
+import {update} from "ramda";
+
+import type {Article} from "../domain/Article";
+
+export type ArticleState = Article[];
+
+export const addArticle = (articleState: ArticleState, article: Article) => articleState.concat(article);
+
+export const removeArticle = (articleState: ArticleState, article: Article) =>
+  articleState.filter((a: Article) => a.id !== article.id);
+
+export const updateArticle = (articleState: ArticleState, article: Article) => {
+  const index = articleState.findIndex((a: Article) => a.id === article.id);
+  return update(index, article, articleState);
+};
+
+export const subscribe = (subscribers: Function[], subscriber: Function): Function[] =>
+  subscribers.concat(subscriber);
+
+export const unsubscribe = (subscribers: Function[], subscriber: Function): Function[] =>
+  subscribers.filter((s: Function) => s !== subscriber);
+
+export const notify = (articleState: ArticleState, subscribers: Function[]) =>
+  subscribers.forEach((s: Function) => s(articleState));
+
+export const articleStateService = (() => {
+  let articleState: ArticleState = Object.freeze([]);
+  let subscribers: Function[] = Object.freeze([]);
+
+  return {
+    addArticle: (article: Article) => {
+      articleState = addArticle(articleState, article);
+      notify(articleState, subscribers);
+    },
+    removeArticle: (article: Article) => {
+      articleState = removeArticle(articleState, article);
+      notify(articleState, subscribers);
+    },
+    updateArticle: (article: Article) => {
+      articleState = updateArticle(articleState, article);
+      notify(articleState, subscribers);
+    },
+    subscribe: (subscriber: Function) => {
+      subscribers = subscribe(subscribers, subscriber);
+    },
+    unsubscribe: (subscriber: Function) => {
+      subscribers = unsubscribe(subscribers, subscriber);
+    }
+  }
+})();
